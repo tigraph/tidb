a14f2f55afb32b61b29d25042dac5de4de2a4152
二手掉包工程师 <rustin.liu@gmail.com>
2022-01-01 17:34:36 +0800

    codec: support write graph records (#9)

--- ddl/ddl_api.go
+++ ddl/ddl_api.go
@@ -1901,32 +1901,9 @@ func buildTableInfoWithStmt(ctx sessionctx.Context, s *ast.CreateTableStmt, dbCh
 	}
 
 	tbInfo.Type = s.Type
-	for _, cd := range colDefs {
-		if len(cd.Options) == 0 {
-			continue
-		}
-		for _, opt := range cd.Options {
-			switch opt.Tp {
-			case ast.ColumnOptionSourceKey:
-				if tbInfo.SourceVertex != nil {
-					return nil, errors.Errorf("Only one SOURCE KEY can be specified")
-				}
-				tbInfo.SourceVertex = &model.EdgeReference{Schema: opt.Refer.Table.Schema, Vertex: opt.Refer.Table.Name}
-			case ast.ColumnOptionDestinationKey:
-				if tbInfo.DestinationVertex != nil {
-					return nil, errors.Errorf("Only one DESTINATION KEY can be specified")
-				}
-				tbInfo.DestinationVertex = &model.EdgeReference{Schema: opt.Refer.Table.Schema, Vertex: opt.Refer.Table.Name}
-			}
-		}
-	}
-	if tbInfo.Type == model.TableTypeIsEdge {
-		if tbInfo.SourceVertex == nil {
-			return nil, errors.Errorf("SOURCE KEY column is missing")
-		}
-		if tbInfo.DestinationVertex == nil {
-			return nil, errors.Errorf("DESTINATION KEY column is missing")
-		}
+	err = checkGraphInfo(tbInfo, colDefs)
+	if err != nil {
+		return nil, errors.Trace(err)
 	}
 
 	if err = setTableAutoRandomBits(ctx, tbInfo, colDefs); err != nil {
@@ -1966,6 +1943,96 @@ func buildTableInfoWithStmt(ctx sessionctx.Context, s *ast.CreateTableStmt, dbCh
 	}
 
 	return tbInfo, nil
+}
+
+func checkGraphInfo(tbInfo *model.TableInfo, colDefs []*ast.ColumnDef) error {
+	var err error
+	switch tbInfo.Type {
+	case model.TableTypeIsVertex:
+		err = checkGraphVertexInfo(tbInfo)
+	case model.TableTypeIsEdge:
+		err = checkGraphEdgeInfo(tbInfo, colDefs)
+	default:
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	if tbInfo.IsCommonHandle || tbInfo.PKIsHandle {
+		return errors.New("can not specified primary key on vertex")
+	}
+	switch tbInfo.Type {
+	case model.TableTypeIsVertex:
+		tbInfo.PKIsHandle = true
+		tbInfo.Columns[0].Flag |= mysql.PriKeyFlag
+		tbInfo.Columns[0].Flag |= mysql.NotNullFlag
+	case model.TableTypeIsEdge:
+		tbInfo.IsCommonHandle = true
+		tbInfo.Columns[0].Flag |= mysql.PriKeyFlag
+		tbInfo.Columns[0].Flag |= mysql.NotNullFlag
+		tbInfo.Columns[1].Flag |= mysql.PriKeyFlag
+		tbInfo.Columns[1].Flag |= mysql.NotNullFlag
+		idxInfo := &model.IndexInfo{
+			Name: model.NewCIStr(mysql.PrimaryKeyName),
+			Columns: []*model.IndexColumn{
+				{
+					Name:   model.NewCIStr(tbInfo.Columns[0].Name.O),
+					Offset: 0,
+					Length: types.UnspecifiedLength,
+				},
+				{
+					Name:   model.NewCIStr(tbInfo.Columns[1].Name.O),
+					Offset: 1,
+					Length: types.UnspecifiedLength,
+				},
+			},
+			Unique:  true,
+			Primary: true,
+			State:   model.StatePublic,
+		}
+		tbInfo.Indices = append(tbInfo.Indices, idxInfo)
+	}
+	return nil
+}
+
+func checkGraphVertexInfo(tbInfo *model.TableInfo) error {
+	if tbInfo.Type == model.TableTypeIsVertex {
+		if tbInfo.Columns[0].Name.L != "vertex_id" || tbInfo.Columns[0].Tp != mysql.TypeLonglong {
+			return errors.Errorf("the first column of vertex should be 'vertex_id bigint'")
+		}
+	}
+	return nil
+}
+
+func checkGraphEdgeInfo(tbInfo *model.TableInfo, colDefs []*ast.ColumnDef) error {
+	for _, cd := range colDefs {
+		if len(cd.Options) == 0 {
+			continue
+		}
+		for _, opt := range cd.Options {
+			switch opt.Tp {
+			case ast.ColumnOptionSourceKey:
+				if tbInfo.SourceVertex != nil {
+					return errors.Errorf("Only one SOURCE KEY can be specified")
+				}
+				tbInfo.SourceVertex = &model.EdgeReference{Schema: opt.Refer.Table.Schema, Vertex: opt.Refer.Table.Name}
+			case ast.ColumnOptionDestinationKey:
+				if tbInfo.DestinationVertex != nil {
+					return errors.Errorf("Only one DESTINATION KEY can be specified")
+				}
+				tbInfo.DestinationVertex = &model.EdgeReference{Schema: opt.Refer.Table.Schema, Vertex: opt.Refer.Table.Name}
+			}
+		}
+	}
+	if tbInfo.Type == model.TableTypeIsEdge {
+		if tbInfo.SourceVertex == nil {
+			return errors.Errorf("SOURCE KEY column is missing")
+		}
+		if tbInfo.DestinationVertex == nil {
+			return errors.Errorf("DESTINATION KEY column is missing")
+		}
+	}
+	return nil
 }
 
 func (d *ddl) assignTableID(tbInfo *model.TableInfo) error {
--- table/tables/tables.go
+++ table/tables/tables.go
@@ -155,6 +155,9 @@ func TableFromMeta(allocs autoid.Allocators, tblInfo *model.TableInfo) (table.Ta
 		if tblInfo.TableCacheStatusType != model.TableCacheStatusDisable {
 			return newCachedTable(&t)
 		}
+		if tblInfo.Type == model.TableTypeIsVertex || tblInfo.Type == model.TableTypeIsEdge {
+			return &GraphCommon{t}, nil
+		}
 		return &t, nil
 	}
 	return newPartitionedTable(&t, tblInfo)
